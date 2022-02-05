37088dc6fd80f6166809e010448199464952ca53
ishaan-sangwan <69313039+ishaan-sangwan@users.noreply.github.com>
2022-02-05 16:19:48 +0530

    Update 2d_array.c

--- 2d_array.c
+++ 2d_array.c
@@ -6,7 +6,7 @@ int main() {
     int tr_matrix[3][3];
     for (i=0; i<3;i++){
         for (j=0;j<3;j++){
-            printf("please enter the the eleemet");
+            printf("please enter the the element");
             scanf("%d", &matrix[i][j]);
         }
     }
