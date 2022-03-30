7c774e1445ba6a1e2a37843b1a06611c0d84d7e3
ishaan sangwan <ishaansangwan@gmail.com>
2022-03-30 16:21:53 +0530

    now printing the tables in the database

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 import psycopg2 as sql
+import os
+import sys
 con = sql.connect(host="localhost", dbname='ishaan', user='postgres', password="123") 
 cur = con.cursor()
 
@@ -6,11 +8,17 @@ cur = con.cursor()
 in con created a cursor named cur"""
 try:
     # cur.execute("DROP TABLE ishaan_table")
-    cur.execute("CREATE TABLE ishaan_table(name varchar(30) NOT NULL primary key, date  date )")
+    # cur.execute("CREATE TABLE ishaan_table(name varchar(30) NOT NULL primary key, date  date )")
     con.commit()
-    # cur.execute("SELECT * FROM pg_catalog.pg_tables;")
+
+    cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+    out = cur.fetchall()
+    for i in out:
+        print(i)
+
     """created new table named ishaan _table in database ishaan
     and changes are comitted in connection"""
+
 
 except Exception as e:
     con.rollback()
