5576060c917e45432d0c01d76dc1ce7acad1278e
ishaan sangwan <ishaansangwan@gmail.com>
2022-03-31 11:25:26 +0530

    done codinfg

--- __init__.py
+++ __init__.py
@@ -10,10 +10,10 @@ cur = con.cursor()
 """created a connection betweeen python and postgres and named it con 
 in con created a cursor named cur"""
 
-def json_column_name(json_file):
-    f = open(json_file)
+def json_column_name(config_file):
+    f = open(config_file)
     data = js.load(f)
-    column_name = list(data[0].keys())
+    column_name = list(data.keys())
     f.close()
     print(column_name)
     return column_name
@@ -36,23 +36,31 @@ def json_column_types(config_file):
 
 def creating_table(config_file):
     f1 = open(config_file)
-    config = js.load(config_file)
-    cur.execute("CREATE TABLE test('s_no numeric primary key')")
+    config = js.load(f1)
+    cur.execute("CREATE TABLE  test (sno numeric);")
     for i in config:
-        cur.execute("ALTER TABLE test ADD COLUMN {} {}".format(i, config[i]))
+        cur.execute("ALTER TABLE test ADD COLUMN {} {};".format(i, config[i]))
         con.commit()
-    f.close()
+    cur.execute("ALTER TABLE test DROP COLUMN sno;")
+    con.commit()
+    f1.close()
 """ new function to create table and adding all the columns by iterating through the config file"""
 
 
 def data_entry(data_file, column_names):
-    try:
-        f = open("distros.json")
-        Data_File = js.load(f)
-        for i in Data_File:
-            values = list(Data_File.values())
-            cur.execute("INSERT INTO test", str(column_names)[1:-1], "VALUES",str(values)[1:-1])
-        f.close()
-    except Exception as e:
-        print(e)
     
+    f = open(data_file)
+    Data_File = js.load(f)
+    c_name = str(column_names)[1:-1]
+    
+    c_name = c_name.replace("'", "")
+    for i in range(len(Data_File)):
+        values = list(Data_File[i].values())
+        val = str(values)[1:-1]
+        cur.execute("INSERT INTO test ("+ c_name + ") VALUES ("+ val +")")
+        con.commit()
+    f.close()
+    
+c_names = json_column_name("dbconfig.json")
+creating_table("dbconfig.json")
+data_entry("distros.json", c_names)
